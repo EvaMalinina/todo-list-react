23dbb4b79140ee7aeb3150c6ea8c9fb8b67d5e93
Tatyana Dotsenko <userdotsenko@gmail.com>
2020-03-17 23:02:15 +0200

    onClick actions added

--- src/components/filter-panel.js
+++ src/components/filter-panel.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const FilterPanel = () => {
-  return (
-    <div className="filter">
-      <ul className="filter__list">
-        <li className="filter__item">
-          <button className="filter__btn active">all</button>
-        </li>
-        <li className="filter__item">
-          <button className="filter__btn">active</button>
-        </li>
-        <li className="filter__item">
-          <button className="filter__btn">done</button>
-        </li>
-      </ul>
-    </div>
-  )
+export default class FilterPanel extends Component {
+  
+  render() {
+    return (
+      <div className="filter">
+        <ul className="filter__list">
+          <li className="filter__item">
+            <button className="filter__btn active">all</button>
+          </li>
+          <li className="filter__item">
+            <button className="filter__btn">active</button>
+          </li>
+          <li className="filter__item">
+            <button className="filter__btn">done</button>
+          </li>
+        </ul>
+      </div>
+    )
+  }
 };
-
-export default FilterPanel;
--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/main.scss'
 
-const TodoItem = ({ label, important = false }) => {
+export default class TodoItem extends Component {
 
-  const style = {
-    color: important ? 'tomato' : 'black'
-  };  
+  constructor() {
+    super();
 
-  return (
-    <div className="item">
-      <span style={style}>{ label }</span>
-      <div className="item__btns">
-        <a className="item__icon" href="#">
-          <i className="fas fa-trash-alt"></i>
-        </a>
-        <a className="item__icon" href="#">
-          <i className="fas fa-exclamation"></i>
-        </a>
-      </div>
-    </div>
-    
-  )
+    this.onLabelClick = () => {
+      this.setState({
+        done: true
+      });
+    };
+
+    this.onMarkImportant = (e) => {
+      e.preventDefault();
+      this.setState({
+        important: true
+      });
+    };
+
+    this.state = {
+      done: false,
+      important: false
+    };
+  }
+
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'item';
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += ' important';
+    }
+
+    // const style = {
+    //   color: important ? 'tomato' : 'black',
+    //   fontWeight: important ? 'bold' : 'normal'
+    // };  
+
+    return (
+      <div className={ classNames }>
+        <span onClick={ this.onLabelClick } >{ label }</span>
+        <div className="item__btns">
+          <a href="google.com" className="item__icon">
+            <i className="fas fa-trash-alt"></i>
+          </a>
+          <a href="google.com" 
+            className="item__icon"
+            onClick={ this.onMarkImportant }
+          >
+            <i className="fas fa-exclamation"></i>
+          </a>
+        </div>
+      </div> 
+    )
+  }
 };
-
-export default TodoItem;
