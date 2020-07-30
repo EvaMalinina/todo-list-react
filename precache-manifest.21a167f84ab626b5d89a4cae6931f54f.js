53dfde491770c80b67494226e945a7fb21768901
Tatyana Dotsenko <tetiana_dotsenko@epam.com>
2020-07-30 11:40:10 +0300

    Updates

---
+++ precache-manifest.21a167f84ab626b5d89a4cae6931f54f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28ff08b6d71d122947f1f6df84dbf74",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "dec3c357fb50548460ea",
+    "url": "/todo-list-react/static/css/main.fdd1c320.chunk.css"
+  },
+  {
+    "revision": "7217a7c9a21e6260e0f3",
+    "url": "/todo-list-react/static/js/2.8f2daa37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.8f2daa37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec3c357fb50548460ea",
+    "url": "/todo-list-react/static/js/main.43fcc341.chunk.js"
+  },
+  {
+    "revision": "67bd10ae112730dd0d0a",
+    "url": "/todo-list-react/static/js/runtime-main.009d09c7.js"
+  }
+]);
